20af64b8661280caf76a2e47c2714b23d90abb9c
qbisi <qbisicwate@gmail.com>
2024-09-15 15:09:44 +0800

    refactor

--- modules/disko/btrfs.nix
+++ modules/disko/btrfs.nix
@@ -1,18 +1,4 @@
 { config, lib, ... }:
-let
-  device = config.boot.loader.grub.device;
-  partitions =
-    if config.disko.biosboot.enable then
-      {
-        boot = {
-          size = "1M";
-          type = "EF02";
-          priority = 0;
-        };
-      }
-    else
-      { };
-in
 {
   options = {
     disko.biosboot.enable = lib.mkOption {
@@ -25,66 +11,77 @@ in
     };
   };
 
-  config = {
-    disko = {
-      enableConfig = true;
-      devices = {
-        disk.main = {
-          name = config.disko.type;
-          imageSize = "2G";
-          device = if (device != "") then device else "/dev/disk/by-diskseq/1";
-          type = "disk";
-          content = {
-            type = "gpt";
-            partitions = partitions // {
-              ESP = {
-                name = "ESP";
-                size = "4M";
-                type = "EF00";
-                priority = 1;
-                content = {
-                  type = "filesystem";
-                  format = "vfat";
-                  mountpoint = "/boot/efi";
-                  mountOptions = [
-                    "fmask=0077"
-                    "dmask=0077"
-                  ];
+  config =
+    let
+      device = config.boot.loader.grub.device;
+    in
+    {
+      disko = {
+        enableConfig = true;
+        devices = {
+          disk.main = {
+            name = config.disko.type;
+            imageSize = "2G";
+            device = if (device != "") then device else "/dev/disk/by-diskseq/1";
+            type = "disk";
+            content = {
+              type = "gpt";
+              partitions = (lib.optionalAttrs config.disko.biosboot.enable {
+                boot = {
+                  size = "1M";
+                  type = "EF02";
+                  priority = 0;
                 };
-              };
-              nix = {
-                size = "100%";
-                content = {
-                  type = "btrfs";
-                  extraArgs = [ "-f" ];
-                  subvolumes = {
-                    "/" = {
-                      mountOptions = [ "noatime" ];
-                      mountpoint = "/.btrfs_root";
-                    };
-                    "/@" = {
-                      mountOptions = [
-                        "compress=zstd"
-                        "noatime"
-                      ];
-                      mountpoint = "/";
-                    };
-                    "/@var" = {
-                      mountOptions = [
-                        "compress=zstd"
-                        "noatime"
-                      ];
-                      mountpoint = "/var";
-                    };
-                    "/@home" = {
-                      mountOptions = [
-                        "compress=zstd"
-                        "noatime"
-                      ];
-                      mountpoint = "/home";
-                    };
-                    "/@swap" = {
-                      mountpoint = "/swap";
+              }) // {
+                ESP = {
+                  name = "ESP";
+                  size = "4M";
+                  type = "EF00";
+                  priority = 1;
+                  content = {
+                    type = "filesystem";
+                    format = "vfat";
+                    mountpoint = "/boot/efi";
+                    mountOptions = [
+                      "fmask=0077"
+                      "dmask=0077"
+                    ];
+                  };
+                };
+                nix = {
+                  size = "100%";
+                  content = {
+                    type = "btrfs";
+                    extraArgs = [ "-f" ];
+                    subvolumes = {
+                      "/" = {
+                        mountOptions = [ "noatime" ];
+                        mountpoint = "/.btrfs_root";
+                      };
+                      "/@" = {
+                        mountOptions = [
+                          "compress=zstd"
+                          "noatime"
+                        ];
+                        mountpoint = "/";
+                      };
+                      "/@var" = {
+                        mountOptions = [
+                          "compress=zstd"
+                          "noatime"
+                        ];
+                        mountpoint = "/var";
+                      };
+                      "/@home" = {
+                        mountOptions = [
+                          "compress=zstd"
+                          "noatime"
+                        ];
+                        mountpoint = "/home";
+                      };
+                      "/@swap" = {
+                        mountpoint = "/swap";
+                      };
                     };
                   };
                 };
@@ -94,5 +91,4 @@ in
         };
       };
     };
-  };
 }
