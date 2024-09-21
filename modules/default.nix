839b6fa212c1266124dbecf11f0770abfe8a7f0d
qbisi <qbisicwate@gmail.com>
2024-09-22 07:39:19 +0800

    fix

--- modules/default.nix
+++ modules/default.nix
@@ -7,7 +7,6 @@
         disabledModules = [ "system/boot/loader/grub/grub.nix" ];
 
         imports = [
-          ./disko/uboot.nix
           ./disko/profile.nix
           ./disko/btrfs.nix
           ./overlay/system/boot/loader/grub.nix
@@ -18,6 +17,7 @@
       bootstrap = { config, pkgs, modulesPath, ... }: {
         imports = [
           "${modulesPath}/profiles/all-hardware.nix"
+          ./disko/uboot.nix
           ./config/networking.nix
           ./system/grow-partition.nix
           ./config/passless.nix
