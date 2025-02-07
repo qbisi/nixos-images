c04f9aa35e87b1fd726b030498b326a3a5f2186a
qbisi <qbisicwate@gmail.com>
2025-02-08 04:06:14 +0800

    pkgs: add rockchip64 kernel

---
+++ pkgs/kernels/linux-rockchip64-6.12.nix
@@ -0,0 +1,51 @@
+{
+  lib,
+  buildLinux,
+  fetchurl,
+  fetchFromGitHub,
+  makePatch,
+  linux_6_12,
+  ...
+}:
+let
+  version = linux_6_12.modDirVersion;
+  modDirVersion = version;
+  src = linux_6_12.src;
+  armbianBuild = fetchFromGitHub {
+    owner = "qbisi";
+    repo = "build";
+    rev = "d86616fd67c980e4d1df48232e826f20d6f72fbf";
+    nonConeMode = true;
+    sparseCheckout = [
+      "config/kernel/*.config"
+      "patch/kernel/**/*.patch"
+    ];
+    hash = "sha256-OQpdLcTo83c+ZUy0fNTNelKxCoyWeF1Zj4S0Q4CAt+k=";
+  };
+  defconfigFile = "${armbianBuild}/config/kernel/linux-rockchip64-current.config";
+  patchDir = "${armbianBuild}/patch/kernel/archive/rockchip64-6.12";
+  kernelPatches = (
+    map (p: {
+      name = baseNameOf p;
+      patch = p;
+    }) (lib.filesystem.listFilesRecursive patchDir)
+  );
+  structuredExtraConfig = with lib.kernel; {
+    # hdmi display phy
+    PHY_ROCKCHIP_SAMSUNG_HDPTX = yes;
+    # pcie phy
+    PHY_ROCKCHIP_SNPS_PCIE3 = yes;
+  };
+in
+buildLinux {
+  inherit
+    src
+    modDirVersion
+    version
+    defconfigFile
+    kernelPatches
+    structuredExtraConfig
+    ;
+  ignoreConfigErrors = true;
+  autoModules = true;
+}
--- pkgs/top-level.nix
+++ pkgs/top-level.nix
@@ -4,6 +4,8 @@ let
   packages = rec {
     linux_rkbsp_joshua = callPackage ./kernels/linux-rkbsp-joshua.nix { };
     linux_phytium_6_6 = callPackage ./kernels/linux-phytium-6.6.nix { };
+    linux_rockchip64_6_13 = callPackage ./kernels/linux-rockchip64-6.13.nix { };
+    linux_rockchip64_6_12 = callPackage ./kernels/linux-rockchip64-6.12.nix { };
     mali_panthor_g610-firmware = callPackage ./mali-panthor-g610-firmware.nix { };
     brcmfmac_sdio-firmware = callPackage ./brcmfmac_sdio-firmware.nix { };
     makePatch = callPackage ../pkgs/makePatch.nix { };
