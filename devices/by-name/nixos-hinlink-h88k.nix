2f5413394ec6ab1e87797bd7adbc3ead86b895e9
qbisi <qbisicwate@gmail.com>
2025-04-04 19:21:28 +0800

    h88k add led rules

--- devices/by-name/nixos-hinlink-h88k.nix
+++ devices/by-name/nixos-hinlink-h88k.nix
@@ -48,9 +48,34 @@
     };
   };
 
+  services.udev.packages = [
+    (pkgs.writeTextFile {
+      name = "naming-audios-udev-rules";
+      destination = "/etc/udev/rules.d/90-naming-audios.rules";
+      text = ''
+        SUBSYSTEM=="sound", ENV{ID_PATH}=="platform-hdmi0-sound", ENV{SOUND_DESCRIPTION}="HDMI0 Audio"
+        SUBSYSTEM=="sound", ENV{ID_PATH}=="platform-hdmi1-sound", ENV{SOUND_DESCRIPTION}="HDMI1 Audio"
+        SUBSYSTEM=="sound", ENV{ID_PATH}=="platform-hdmiin-sound", ENV{SOUND_DESCRIPTION}="HDMI-In Audio"
+        SUBSYSTEM=="sound", ENV{ID_PATH}=="platform-dp0-sound", ENV{SOUND_DESCRIPTION}="DP0 Audio"
+        SUBSYSTEM=="sound", ENV{ID_PATH}=="platform-es8388-sound", ENV{SOUND_DESCRIPTION}="ES8388 Audio"
+      '';
+    })
+    (pkgs.writeTextFile {
+      name = "setting-led-udev-rules";
+      destination = "/etc/udev/rules.d/90-setting-led.rules";
+      text = ''
+        ACTION=="add", SUBSYSTEM=="leds", KERNEL=="blue:net", ATTR{device_name}="wwan0"
+        ACTION=="add", SUBSYSTEM=="leds", KERNEL=="blue:net", ATTR{link}="1"
+        ACTION=="add", SUBSYSTEM=="leds", KERNEL=="blue:net", ATTR{rx}="1"
+        ACTION=="add", SUBSYSTEM=="leds", KERNEL=="blue:net", ATTR{tx}="1"
+      '';
+    })
+  ];
+
   boot = {
     kernelPackages = pkgs.linuxPackagesFor pkgs.linux_rockchip64_6_14;
     initrd.availableKernelModules = lib.mkForce [ ];
+    kernelModules = [ "ledtrig-netdev" ];
     kernelParams = [
       "console=tty1"
       "earlycon"
