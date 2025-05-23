9a5d55a2d1de5977dd7afc7339a63b66f1b67b06
qbisi <qbisicwate@gmail.com>
2025-05-23 17:22:54 +0800

    armbianBuild: 25.5.0-trunk.505 -> 25.5.1

--- pkgs/armbianBuild.nix
+++ pkgs/armbianBuild.nix
@@ -2,11 +2,11 @@
 fetchFromGitHub {
   owner = "armbian";
   repo = "build";
-  tag = "v25.5.0-trunk.505";
+  tag = "v25.5.1";
   nonConeMode = true;
   sparseCheckout = [
     "config/kernel/*.config"
     "patch/kernel/**/*.patch"
   ];
-  hash = "sha256-PNcTOqGCUo7E9xNZZvmF+g91lFNzMlLnjQM8xguajzs=";
+  hash = "sha256-Xrd2PElurtTtSQ2WqI7GdW0oOs/ZrufHxgwYQ0fD91A=";
 }
