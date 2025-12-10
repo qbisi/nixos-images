837fafec6641c374f1c3222e6c862d6005a733bb
qbisi <qbisicwate@gmail.com>
2025-12-10 23:49:52 +0800

    fix nixpkgs overlay

--- modules/config/nixpkgs.nix
+++ modules/config/nixpkgs.nix
@@ -1,7 +1,6 @@
 { self, inputs, ... }:
 {
   nixpkgs.overlays = [
-    (self.overlays.default or (final: prev: { }))
-    (inputs.nixos-images.overlays.default or (final: prev: { }))
+    (inputs.nixos-images.overlays.default or self.overlays.default or (final: prev: { }))
   ];
 }
