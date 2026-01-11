486c7dd66d4d6823a1295f6a970e1cb3c58da676
qbisi <qbisicwate@gmail.com>
2026-01-11 14:04:57 +0800

    linux_rkbsp: 6.1.118 -> 6.1.115

--- pkgs/linux_rkbsp_6_1.nix
+++ pkgs/linux_rkbsp_6_1.nix
@@ -7,13 +7,13 @@
   ...
 }:
 let
-  version = "6.1.118-armbian";
-  modDirVersion = "6.1.118";
+  version = "6.1.115-armbian";
+  modDirVersion = "6.1.115";
   src = fetchFromGitHub {
     owner = "armbian";
     repo = "linux-rockchip";
-    rev = "b67dc5c9ade9dc354b790eb64aa6a665d0a54ecd";
-    hash = "sha256-xSZ63aUR78V92eD9X+rEYl0+1jRG1lBwhGhE2xjIn2U=";
+    rev = "b908c7339f51eddcfe8402cd15d1e1f8f4e67c29";
+    hash = "sha256-70wGP16SJHs7I8HklhNdrJbWzfvcgJCupgfOq81e1U8=";
   };
   defconfigFile = "${armbianBuild}/config/kernel/linux-rk35xx-vendor.config";
 in
