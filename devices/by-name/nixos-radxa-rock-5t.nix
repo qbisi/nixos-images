98c06a9fbc659acd0d9dcdb6ddd9449bc6c33f86
qbisi <qbisicwate@gmail.com>
2026-05-11 15:29:13 +0800

    init rock5t support

---
+++ devices/by-name/nixos-radxa-rock-5t.nix
@@ -0,0 +1,82 @@
+{
+  config,
+  pkgs,
+  lib,
+  inputs,
+  self,
+  ...
+}:
+{
+  nixpkgs = {
+    system = "aarch64-linux";
+  };
+
+  networking.hostName = lib.mkDefault "r5t";
+
+  disko = {
+    enableConfig = true;
+    bootImage = {
+      enableESP = false;
+      fileSystem = "ext4";
+      primaryStart = "16M";
+      uboot.enable = true;
+      uboot.package = pkgs.buildUBootRk3588 {
+        dtsFile = ../../dts/mainline/rockchip/rk3588-rock-5t.dts;
+      };
+    };
+  };
+
+  hardware = {
+    firmware = [
+      (pkgs.armbian-firmware.override {
+        filters = [
+          "arm/mali/*"
+          "rtl_nic/*"
+          "mediatek/*"
+          "ap6275p/*"
+          "brcm/*"
+          "updates/*"
+          "regulatory.db"
+        ];
+      })
+    ];
+    deviceTree = {
+      name = "rockchip/rk3588-rock-5t.dtb";
+    };
+    serial = {
+      enable = true;
+      unit = 2;
+      baudrate = 1500000;
+    };
+  };
+
+  environment = {
+    variables = {
+      MESA_GLSL_VERSION_OVERRIDE = 330;
+      ALSA_CONFIG_UCM2 = "${pkgs.alsa-ucm-conf-rk3588}/share/alsa/ucm2";
+    };
+    systemPackages = with pkgs; [
+      usbutils
+      pciutils
+      i2c-tools
+      libgpiod
+      minicom
+      ethtool
+    ];
+  };
+
+  boot = {
+    kernelPackages = pkgs.linuxPackagesFor pkgs.linux_rockchip64_7_0;
+    initrd.allowMissingModules = !config.boot.kernelPackages.kernel.configfile.autoModules;
+    kernelParams = [
+      "console=tty1"
+      "earlycon"
+      "net.ifnames=0"
+    ];
+    consoleLogLevel = 6;
+    loader.timeout = 0;
+    loader.grub.enable = config.disko.bootImage.enableESP;
+    loader.generic-extlinux-compatible.enable = !config.disko.bootImage.enableESP;
+  };
+
+}
---
+++ hosts/by-name/r5t.nix
@@ -0,0 +1,59 @@
+{
+  config,
+  pkgs,
+  lib,
+  self,
+  inputs,
+  inputs',
+  ...
+}:
+{
+  deployment = {
+    targetHost = "10.0.10.1";
+    targetUser = "root";
+    buildOnTarget = false;
+  };
+
+  imports = [
+    "${self}/devices/by-name/nixos-radxa-rock-5t.nix"
+    "${self}/modules/config/passless.nix"
+  ];
+
+  hardware = {
+    deviceTree = {
+      dtboBuildExtraIncludePaths = lib.mkAfter [ ../../dts/mainline ];
+      overlays = [
+        {
+          name = "mipi-sgm-6fhd";
+          dtsFile = ../../dts/mainline/overlays/rk3588-tianma-display-sgm-6fhd.dtso;
+        }
+      ];
+    };
+    graphics.enable = true;
+  };
+
+  networking = {
+    firewall.enable = false;
+    nftables.enable = true;
+  };
+
+  services = {
+    usb-rndis.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    iperf3
+  ];
+
+  documentation.enable = false;
+
+  nix = {
+    settings = {
+      substituters = lib.mkForce [ ];
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+    };
+  };
+}
