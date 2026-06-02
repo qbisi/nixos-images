ed26c442de45bc3207eeb02beb171d90bfa538b5
qbisi <qbisicwate@gmail.com>
2026-06-02 21:16:12 +0800

    bootstrap: add flakeinput

--- devices/bootstrap.nix
+++ devices/bootstrap.nix
@@ -1,9 +1,22 @@
 {
   config,
   lib,
+  self,
   modulesPath,
   ...
 }:
+let
+  flakeOutPaths =
+    let
+      collector =
+        parent:
+        map (
+          child:
+          [ child.outPath ] ++ (if child ? inputs && child.inputs != { } then (collector child) else [ ])
+        ) (lib.attrValues parent.inputs);
+    in
+    lib.unique (lib.flatten (collector self));
+in
 {
   imports = [
     "${modulesPath}/profiles/minimal.nix"
@@ -13,6 +26,14 @@
   nixpkgs.flake = {
     setFlakeRegistry = false;
     setNixPath = false;
+  };
+
+  system = {
+    extraDependencies = flakeOutPaths;
+    systemBuilderCommands = lib.mkAfter ''
+      mkdir -p $out/nixos-config
+      cp -r ${self}/ $out/nixos-config
+    '';
   };
 
   disko = {
@@ -27,7 +48,7 @@
     };
 
     bootImage = {
-      imageSize = "2G";
+      imageSize = "4G";
       partLabel = lib.mkIf (builtins.getEnv "PARTLABEL" != "") (builtins.getEnv "PARTLABEL");
     };
   };
