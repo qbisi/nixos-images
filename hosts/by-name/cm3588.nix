2125f72788596c5d75d2c6b0f149a0221d0334c3
qbisi <qbisicwate@gmail.com>
2026-06-07 02:58:38 +0800

    fix

--- hosts/by-name/cm3588.nix
+++ hosts/by-name/cm3588.nix
@@ -7,7 +7,7 @@
 }:
 {
   imports = [
-    ../../devices/by-name/nixos-radxa-rock-5t.nix
+    ../../devices/by-name/nixos-friendlyelec-cm3588-nas.nix
     ../../profiles/desktop.nix
     ../../profiles/common.nix
   ];
