87a3a66675f370d6c9700f4857b5f335c54b526f
qbisi <qbisicwate@gmail.com>
2026-06-07 02:59:12 +0800

    fix

--- devices/by-name/nixos-friendlyelec-cm3588-nas.nix
+++ devices/by-name/nixos-friendlyelec-cm3588-nas.nix
@@ -12,20 +12,6 @@
 
   networking.hostName = lib.mkDefault "cm3588";
 
-  disko = {
-    bootImage = {
-      partLabel = lib.mkDefault "NVME";
-      primaryStart = "1M";
-      uboot = {
-        enable = false;
-        package = pkgs.buildUBootRk3588 {
-          withSpi = true;
-          dtsFile = config.hardware.deviceTree.dtsFile;
-        };
-      };
-    };
-  };
-
   hardware = {
     deviceTree = {
       name = "rockchip/rk3588-friendlyelec-cm3588-nas.dtb";
