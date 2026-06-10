30cb8971887f795bf12fc5a4d855ea92671e4480
qbisi <qbisicwate@gmail.com>
2026-06-10 23:51:06 +0800

    patches: u-boot: central menu

--- overlays.nix
+++ overlays.nix
@@ -121,6 +121,7 @@ self: pkgs: {
           ./patches/u-boot/rk3588-adc-recovery.patch
           ./patches/u-boot/cmd-add-bootconfig.patch
           ./patches/u-boot/bootflow-menu-countdown.patch
+          ./patches/u-boot/bootflow-menu-central.patch
           ./patches/u-boot/clk-enhance-clk-gpio-to-also-handle-gated-fixed-clock.patch
         ]
         ++ pkgs.lib.optional withDrm ./patches/u-boot/rockchip-video-drm.patch
